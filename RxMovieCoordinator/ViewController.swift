dddceba0903aae95efaea1a4caa5fc0af0a3d923
Sherif Kamal <sherif.kamal.salem@gmail.com>
2020-01-14 07:22:34 +0200

    Initial Commit

---
+++ RxMovieCoordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxMovieCoordinator
+//
+//  Created by Sherif Kamal on 1/14/20.
+//  Copyright © 2020 Sherif Kamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
